60ccdc89cf7233712bc5a60a5c23219ae68f34bd
hemanthlucifer <hemanthkumar.rfchh@gmail.com>
2025-01-22 21:33:37 +0530

    initial commit

---
+++ src/test/java/com/abcignite/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcignite.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
